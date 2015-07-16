c2b3cd77e89779c18f9ccf54e7fa3b28c730b3ef
Augusto Gortaire <augustogortaire@gmail.com>
2015-07-16 15:35:45 -0500

    crear repositorio y subida de archivos iniciales

---
+++ BUCLES.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# COPYRIGHT (C) 2015 AUGUSTO GORTAIRE C <agortaire@cyg.ec>
+__author__ = 'Augusto Gortaire'
+
+# ESTE PROGRAMA MUESTRA EL USO DEL BUCLE FOR
+# EL USUARIO DIRA CUANTAS VECES SE REPITE EL MENSAJE, Y CUAL MENSAJE
+def imprimir(mensaje):
+    print u""
+
+n = raw_input(u"Número de veces")
+mensaje = raw_input(u"MENSAJE")
+# TRANSFORMO A ENTERI EL NUMERO DE REPETICIONES
+n = int(n)
+
+for i in range(n):
+    print mensaje
+
